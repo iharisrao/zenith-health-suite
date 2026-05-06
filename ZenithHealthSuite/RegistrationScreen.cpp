1ff5b9c91ead3ac523feb0ba3e14faed6423beb2
iharisrao <raomharis07@gmail.com>
2026-05-06 19:36:03 +0500

    made the logic behind registration screen ui

--- ZenithHealthSuite/RegistrationScreen.cpp
+++ ZenithHealthSuite/RegistrationScreen.cpp
@@ -1,12 +1,21 @@
 #include "RegistrationScreen.h"
+#include "ui_RegistrationScreen.h"  //it is seceret file which is created by Qt when we use drag and drop canvas
+#include <QMessageBox>              // tool which is used to make pop up alerts and show on screen
 
-RegistrationScreen::RegistrationScreen(QWidget *parent)
-	: QMainWindow(parent)
+RegistrationScreen::RegistrationScreen(QWidget *parent):
+QWidget(parent),
+ui(new Ui::RegistrationScreen)    //same remote control pointer used in .h file to control the ui elements on the registration screen
 {
-	ui.setupUi(this);
+	ui->setupUi(this);            // magic line which creates the physical lines and boxes on computer screen from your drag and drop design 
 }
 
 RegistrationScreen::~RegistrationScreen()
-{}
+{
+	delete ui;
+}
 
-
+void RegistrationScreen::on_completeRegistrationButton_clicked()
+{
+	QMessageBox::information(this, "System Status", "The complete registration button is alive and connected!"); 
+	// Pop up a success message box right in the middle of this screen
+}
--- ZenithHealthSuite/RegistrationScreen.h
+++ ZenithHealthSuite/RegistrationScreen.h
@@ -1,17 +1,26 @@
-#pragma once
+#pragma once                                 //act like bouncers they stop C++ from accidentally reading this blueprint twice and crashing our app.
+#ifndef RegistrationScreen_h
+#define RegistrationScreen_h
 
-#include <QMainWindow>
-#include "ui_RegistrationScreen.h"
-
-class RegistrationScreen : public QMainWindow
+#include<QWidget>
+namespace Ui {                              // telling c++ that there is registration.ui is available so dont worry about it
+	class RegistrationScreen;
+}
+class RegistrationScreen : public QWidget       //telling program that our app gets all superpower of QWidgets like minimizing etc
 {
-	Q_OBJECT
-
+	Q_OBJECT        //The magic Qt radio antenna! We MUST have this line, otherwise our button clicks will never reach our C++ code.
 public:
-	RegistrationScreen(QWidget *parent = nullptr);
-	~RegistrationScreen();
+	explicit RegistrationScreen(QWidget* parent = nullptr);     //runs when we first open the registration screen
+
+	~RegistrationScreen();     // cleans up memeory when we are done with the registration screen
+
+private slots:
+	void on_completeRegistrationButton_clicked();            //button which waits tills the user clicks it
 
 private:
-	Ui::RegistrationScreenClass ui;
+
+	// whenever we want to read what someone typed into the name or age boxes we have to use this 'ui' pointer to grab it
+	Ui::RegistrationScreen* ui;
 };
 
+#endif         // RegistrationScreen_h
