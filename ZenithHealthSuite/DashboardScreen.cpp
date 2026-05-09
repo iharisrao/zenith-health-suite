da07c31c146f6a7135a658f74bc1860d6dafa50e
aamna <amnashahzad671@gmail.com>
2026-05-10 00:17:15 +0500

    I have enabled the buttons on the dashboard screen UI through logic, so now they will work properly.

--- ZenithHealthSuite/DashboardScreen.cpp
+++ ZenithHealthSuite/DashboardScreen.cpp
@@ -1,11 +1,85 @@
 #include "DashboardScreen.h"
+#include "ui_DashboardScreen.h"
 
-DashboardScreen::DashboardScreen(QWidget *parent)
-	: QMainWindow(parent)
+// We include this because it is the only screen we have actually built!
+#include "DataEntryDialog.h" 
+
+DashboardScreen::DashboardScreen(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::DashboardScreen)
 {
-	ui.setupUi(this);
+    ui->setupUi(this);
 }
 
 DashboardScreen::~DashboardScreen()
-{}
+{
+    delete ui;
+}
 
+// =====================================================================
+// THE ONLY LIVE BUTTON (Because you built the UI for it)
+// =====================================================================
+void DashboardScreen::on_addActivityButton_clicked()
+{
+    qDebug() << "Launching Data Entry Pop-up...";
+    DataEntryDialog popUpWindow(this);
+    popUpWindow.exec(); // Freezes dashboard, opens the pop-up
+}
+
+// =====================================================================
+// PLACEHOLDER NAVIGATION WIRING (Safe to click, won't crash)
+// =====================================================================
+
+void DashboardScreen::on_dashboardButton_clicked() {
+    qDebug() << "You are already on the Dashboard!";
+}
+
+void DashboardScreen::on_analyticsButton_clicked() {
+    qDebug() << "Analytics screen coming soon...";
+    // Later Amna will add: AnalyticsScreen window(this); window.exec();
+}
+
+void DashboardScreen::on_activityButton_clicked() {
+    qDebug() << "Activity log screen coming soon...";
+}
+
+void DashboardScreen::on_goalsButton_clicked() {
+    qDebug() << "Goals screen coming soon...";
+}
+
+void DashboardScreen::on_historyButton_clicked() {
+    qDebug() << "History screen coming soon...";
+}
+
+void DashboardScreen::on_settingsButton_clicked() {
+    qDebug() << "Settings screen coming soon...";
+}
+
+void DashboardScreen::on_supportButton_clicked() {
+    qDebug() << "Support screen coming soon...";
+}
+
+void DashboardScreen::on_buddyButton_clicked() {
+    qDebug() << "Buddy AI screen coming soon...";
+}
+
+void DashboardScreen::on_premiumButton_clicked() {
+    qDebug() << "Premium checkout coming soon...";
+}
+
+void DashboardScreen::on_profileButton_clicked() {
+    qDebug() << "User profile coming soon...";
+}
+
+void DashboardScreen::on_bellButton_clicked() {
+    qDebug() << "No new notifications.";
+}
+
+// =====================================================================
+// BUDDY'S BACKEND CONNECTION FUNCTION
+// =====================================================================
+// When Buddy loads the database, she calls this function to fill your UI cards!
+void DashboardScreen::updateDashboardMetrics(int calories, int steps, int heartRate)
+{
+    // buddy code will come here
+}
--- ZenithHealthSuite/DashboardScreen.h
+++ ZenithHealthSuite/DashboardScreen.h
@@ -1,17 +1,45 @@
-#pragma once
+#ifndef DASHBOARDSCREEN_H
+#define DASHBOARDSCREEN_H
 
 #include <QMainWindow>
-#include "ui_DashboardScreen.h"
+#include <QDebug> 
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class DashboardScreen; }
+QT_END_NAMESPACE
 
 class DashboardScreen : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	DashboardScreen(QWidget *parent = nullptr);
-	~DashboardScreen();
+    explicit DashboardScreen(QWidget* parent = nullptr);
+    ~DashboardScreen();
+
+    void updateDashboardMetrics(int calories, int steps, int heartRate);
+
+private slots:
+    
+    void on_dashboardButton_clicked();
+    void on_analyticsButton_clicked();
+    void on_activityButton_clicked();
+    void on_goalsButton_clicked();
+    void on_historyButton_clicked();
+
+    
+    void on_addActivityButton_clicked();
+
+    void on_settingsButton_clicked();
+    void on_supportButton_clicked();
+    void on_buddyButton_clicked();
+    void on_premiumButton_clicked();
+
+    
+    void on_profileButton_clicked();
+    void on_bellButton_clicked(); 
 
 private:
-	Ui::DashboardScreenClass ui;
+    Ui::DashboardScreen* ui;
 };
 
+#endif // DASHBOARDSCREEN_H
