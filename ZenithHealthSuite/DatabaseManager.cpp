2c4700e15668fa67a1cf7b6d8a2f2958f04c9035
marwahayat023-lab <marwahayat023@gmail.com>
2026-05-12 15:02:31 +0500

    created database for dashboard screen

--- ZenithHealthSuite/DatabaseManager.cpp
+++ ZenithHealthSuite/DatabaseManager.cpp
@@ -1,7 +1,7 @@
 #include "DatabaseManager.h"
 #include <QDebug>
-#include <QCryptographicHash>
-
+#include <QCryptographicHash>   //to protect password 
+#include<QVariant> // help us convert data base valuues into int
 DatabaseManager::DatabaseManager()
 {
     setupDatabase();
@@ -92,3 +92,101 @@ bool DatabaseManager::saveUserRegistration(const QString& username, const QStrin
     }
     return true;
 }
+//////DASHBOARD FUNCTION START HERE//////////
+void DatabaseManager::createDashboardTables() {
+    QSqlQuery query;
+    //creating dailymetrics tables for the top cards
+    QString createDailyMetricsQuery =
+        "CREATE TABLE IF NOT EXISTS DailyMetrics ("
+        "metric_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "usename TEXT,"
+        "record_data TEXT,"
+        "calories_consumed INTEGER,"
+        "calories_goal INTEGER,"
+        "protien_g INTEGER,"
+        "carbs_g INTEGER,"
+        "fats_g INTEGER,"
+        "active_energy INTEGER,"
+        "step_count INTEGER,"
+        "sleep_hours INTEGER,"
+        "sleep_minutes INTEGER,"
+        "recovery_percentage INTEGER,"
+        "heart_rate INTEGER,"
+        "hydration_status TEXT,"
+        "recovery_status TEXT,"
+        "stress_status TEXT,"
+       ")";
+        
+
+    if (!query.exec(createDailyMetricsQuery)) {
+        qDebug() << "Failed to create DailyMetrics table:" << query.lastError().text();
+        
+    }
+    ///create the activitylog table for the bottom table weight
+    QString createActivityQuery =
+        "CREATE TABLE IF NOT EXIST ActivityLog("
+        "activity_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "user_name TEXT,"
+        "record_date TEXT,"
+        "exercise_name TEXT,"
+        "duration_mins INTEGER,"
+        "intensity TEXT,"
+        "calories_burned INTEGER,"
+        "sync_status TEXT )";
+    if (!query.exec(createActivityQuery)) {
+        qDebug() << " Failed to create ActivityLog table:" << query.lastError().text();
+        
+    }
+ 
+}
+DailyMetrics DatabaseManager::getDailyMetrics(const QString& username, const QDate& date) {
+    DailyMetrics data = { 0 };
+    QSqlQuery query (db);
+    query.prepare("Select*from DailyMetrics where username= :username AND record_date=:date");
+    query.bindValue(":username", username);
+    query.bindValue(":date" ,date.toString("yyyy_MM_dd"));
+    if (query.exec() && query.next()) {
+        data.caloriesConsumed = query.value("calories_goal").toInt();
+        data.protien = query.value("protien_g").toInt();
+        data.carbs = query.value("carbs_g").toInt();
+        data.fats = query.value("fats_g").toInt();
+        data.activeEnergy = query.value("active_energy").toInt();
+        data.stepCount = query.value("step_Count").toInt();
+        data.sleepHours = query.value("sleep_Hours").toInt();
+        data.sleepMinutes = query.value("sleep_minutes").toInt();
+        data.recoveryPercentage = query.value("recovery_perecentage").toInt();
+        data.heartRate = query.value("heart_rate").toInt();
+        data.hydrationStatus = query.value("hydration_status").toString();
+        data.recoveryStatus = query.value("recovery_status").toString();
+        data.stressStatus = query.value("stress_status").toString();
+    }
+    else {
+        qDebug() << "No metrics found for user:" << username << "on date:" << date.toString("yyyy-MM-dd");
+    }
+    return data;
+}
+QList<ActivityRecord>DatabaseManager::getActivityLog(const QString& username, const QDate& date){
+    QList<ActivityRecord>logList;
+    QSqlQuery query;
+
+    query.prepare("select * from ActivityLog WHERE username=:username and record_date=:date");
+    query.bindValue(":username", username);
+    query.bindValue(":date", date.toString("yyyy-MM-dd"));
+    if (!query.exec()) {
+        while (query.next()) {
+            ActivityRecord record;
+            record.exerciseName = query.value("exercise_name").toString();
+            record.durationMins = query.value("duration_mins").toInt();
+            record.intensity = query.value("intensity").toString();
+            record.caloriesBurned = query.value("caloriesburned").toInt();
+            record.syncStatus = query.value("sync_status").toString();
+
+            logList.append(record);
+        }
+    }
+    else {
+        qDebug() << "Failed to fetch activity log:" << query.lastError().text();
+    }
+    return logList;
+}
+
--- ZenithHealthSuite/DatabaseManager.h
+++ ZenithHealthSuite/DatabaseManager.h
@@ -4,6 +4,34 @@
 #include <QtSql/QSqlDatabase>
 #include<QtSql/QSqlQuery>
 #include<QtSql/QSqlError>
+#include<QDate>
+#include<QList>
+
+//Dashboard structure
+struct DailyMetrics {
+	int caloriesConsumed;
+	int caloriesGoal;
+	int protien;
+	int carbs;
+	int fats;
+	int activeEnergy;
+	int stepCount;
+	int sleepHours;
+	int sleepMinutes;
+	int recoveryPercentage;
+	int heartRate;
+	QString hydrationStatus;
+	QString recoveryStatus;
+	QString stressStatus;
+};
+
+struct ActivityRecord {
+	QString exerciseName;
+	int durationMins;
+	int caloriesBurned;
+	QString intensity;
+	QString syncStatus;
+};
 
 class DatabaseManager {
 public:
@@ -25,6 +53,10 @@ public:
 		const QString& medicalHistory,
 		bool consent
 	);
+
+	void createDashboardTables();
+	DailyMetrics getDailyMetrics(const QString&  username, const QDate& date);
+	QList<ActivityRecord> getActivityLog(const QString& username, const QDate& date);
 private:
 	QSqlDatabase db;
 	bool setupDatabase();
