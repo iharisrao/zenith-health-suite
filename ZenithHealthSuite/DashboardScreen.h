1211b5e0eaafdc97626432537522a0c05ea12abe
iharisrao <raomharis07@gmail.com>
2026-05-30 22:49:05 +0500

    make changes to database

--- ZenithHealthSuite/DashboardScreen.h
+++ ZenithHealthSuite/DashboardScreen.h
@@ -1,34 +1,44 @@
+// guard which ensures that header file in only read once
 #ifndef DASHBOARDSCREEN_H
 #define DASHBOARDSCREEN_H
 
-#include <QMainWindow>
-#include <QDebug>
-#include <QTimer>       
+#include <QMainWindow>  //library which should be include as all main screen are made on this QT library        
+#include <QDebug>         //used to check wheather all apps runs perfectly and if not will show an error 
+#include <QTimer>       //used to add timer feature in our app
 #include <QMessageBox> 
-#include "Activity.h"
+#include "Activity.h"     //as to run dashboard screen it needs all these other screens 
 #include "History.h"
 #include "analytics.h"
 #include "Goals.h"
 #include "DatabaseManager.h"
 #include "user.h" 
 
+
+//when user make ui using drag and drop the QT writes its code automatically here we are telling the compiler to not to mix dashboard screen c++ with its ui
+//namespace acts as a folder so the files donot mix 
 QT_BEGIN_NAMESPACE
 namespace Ui { class DashboardScreen; }
 QT_END_NAMESPACE
 
+//here QMainWindow is used which tells the user that Dashboard Screen is not a simple screen it is QT mai window 
 class DashboardScreen : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT  //magical button which tells qt to that tis screen will work on signal and slots.if we dont use it our buttons will not work
 
 public:
+    //explicit is strict function which tells compiler to not to make changes in code on its user wait for user permission
+    
+    //all features are child on someone else here QWidget and nullptr ensures that main dashboard screen is independent  and assume it as nullptr
     explicit DashboardScreen(const user& currentUserObj, QWidget* parent = nullptr);
     ~DashboardScreen();
-
+    //if changes made on activity screen it will also shown on dashboard scren using this function
     void updateDashboardMetrics(int caloriesConsumed, int caloriesGoal, int steps, int activeEnergy);
 
 
 
 private slots:
+
+    //when the user presses any button it makes an signal and to catch that signal we make a slot function
     void on_dashboardButton_clicked();
     void on_analyticsButton_clicked();
     void on_activityButton_clicked();
@@ -38,11 +48,14 @@ private slots:
     void on_supportButton_clicked();
     void on_profileButton_clicked();
     void on_bellButton_clicked();
-
+    
     void on_syncDataButton_clicked();
 
 private:
+    //Encapsulation
+    //ui is pointing to the drag and drop pointer 
     Ui::DashboardScreen* ui;
+    //dbManager is used to communicate to local database 
     DatabaseManager dbManager;
     user loggedInUser;
 
--- ZenithHealthSuite/DatabaseManager.cpp
+++ ZenithHealthSuite/DatabaseManager.cpp
@@ -1,44 +1,54 @@
-#include "DatabaseManager.h"
-#include <QDebug>
-#include <QCryptographicHash>
-#include <QVariant>
-#include <QSqlError>
-#include <QCoreApplication> 
-#include <QMessageBox>
+#include "DatabaseManager.h"    //coonect header file to our c++ file 
+#include <QDebug>  //QDebug is for printing messages to the console specifcally used for debugging purposes
+#include <QCryptographicHash>  // save password in encrypted form in database 
+#include <QVariant>      // QVareint is special datatype which can hold any type of data and we can convert it to any type we want
+#include <QSqlError>   //gves u access to .lastError() so  find out the last error occur in our database 
+#include <QCoreApplication>   //helps u to find out .exe file location and create database there
+#include <QMessageBox>  //used to show pop up window on screen 
 
 DatabaseManager::DatabaseManager()
 {
-    setupDatabase();
+	setupDatabase();     /* when the constructor runs it automatically calls the setupDatabase function to
+                           initialize the database connection and create necessary tables if they don't exist. 
+                           This ensures that the database is ready for use as soon as an instance of DatabaseManager
+                           is created */
 }
 
 DatabaseManager::~DatabaseManager()
 {
-    
+    //destructor which is empty bcz database automatically closes itself when the app closes 
 }
 
 bool DatabaseManager::setupDatabase()
 {
-    if (QSqlDatabase::contains("qt_sql_default_connection")) {
+    if (QSqlDatabase::contains("qt_sql_default_connection")) {          //qt_sql_default_connection is default name of databaase which QT creates
         db = QSqlDatabase::database("qt_sql_default_connection");
+        //if default database exist we use it , this precents crashes of duplicate connections
     }
     else {
         db = QSqlDatabase::addDatabase("QSQLITE");
+        // it default database doesnot exists we will create new one and here QSQLITE tells QT that we are using SQLite database driver
 
-        QString databasePath = QCoreApplication::applicationDirPath() + "/ZenithHealth.db";
+        QString databasePath = QCoreApplication::applicationDirPath() + "/ZenithHealth.db"; //dynamically telling compiler to find database file on its own
         db.setDatabaseName(databasePath);
 
+		// creating database full path by combining the application directory path and the database file name "ZenithHealth.db". This ensures that the database file is created in the same directory as the application executable, making it easier to manage and access.
+
         qDebug() << "Database initialized at: " << databasePath;
+        // prints the path to the debug console it is useful during development to confirm the database file was found at right location 
     }
 
     if (!db.open()) {
-        qDebug() << "Error: Connection with database failed";
+        qDebug() << "Error: Connection with database failed";  // if the database doesnot open it will show an error 
         return false;
     }
 
-    db.transaction();
+    db.transaction(); // tells compiler to group all database operation runs them if any of them fails undo all of them 
 
-    QSqlQuery query(db);
+    QSqlQuery query(db); // act as sql command runner 
 
+    // R is literal string which allows u to write sql query on multiple lines without using escape sequence 
+    // real means using decimal values
     if (!query.exec(R"(
         CREATE TABLE IF NOT EXISTS User (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -49,7 +59,7 @@ bool DatabaseManager::setupDatabase()
             phone TEXT,
             targetWeight REAL,
             weight REAL,
-            height REAL,
+            height REAL, 
             gender TEXT,
             bloodGroup TEXT,
             dob TEXT,
@@ -58,24 +68,30 @@ bool DatabaseManager::setupDatabase()
         )
     )")) {
         db.rollback();
+        //if create table command fails then undo the transaction
         return false;
     }
 
-    db.commit();
+    db.commit(); // permenently commit the table 
 
-    createDashboardTables();
+    createDashboardTables();  // calling this function which tell compiler to create 5 more tables and then returns true 
     return true;
 }
 
-bool DatabaseManager::saveUserRegistration(const user& newUser)
+bool DatabaseManager::saveUserRegistration(const user& newUser) // taker user variable from user class referring to user.h
 {
     if (!db.isOpen()) {
         QMessageBox::critical(nullptr, "Database Error", "The database is not open.");
         return false;
-    }
+    } // safety check befote making any type of changes in database 
 
     QString hashedPassword = QString(QCryptographicHash::hash(
         newUser.getPassword().toUtf8(), QCryptographicHash::Sha256).toHex());
+    /* 1. takes raw password whihc is readable 
+    * 2. convert it into raw bytes UTF format bcz the hash function needs bytes not text
+    * 3. run a SHA-256 algorithm and then save a encrypted result
+    * 4. convert the binary result into hexa decimal format 
+    */
 
     QSqlQuery query(db);
 
@@ -84,8 +100,11 @@ bool DatabaseManager::saveUserRegistration(const user& newUser)
         VALUES (:username, :password, :name, :email, :phone, :targetWeight, :weight, :height, :gender, :bloodGroup, :dob, :medicalHistory, :consent)
     )");
 
+    //here : placeholder is used which tell the database i will give u username and its detail soon but for know we call it username 
+
+    // here newUser.gotUsername is combination of getter and variable which is taking the value from the c++ and save it in database
     query.bindValue(":username", newUser.getUsername());
-    query.bindValue(":password", hashedPassword);
+    query.bindValue(":password", hashedPassword);   // ensuring that hashed password saves in table 
     query.bindValue(":name", newUser.getName());
     query.bindValue(":email", newUser.getEmail());
     query.bindValue(":phone", newUser.getPhone());
@@ -96,13 +115,14 @@ bool DatabaseManager::saveUserRegistration(const user& newUser)
     query.bindValue(":bloodGroup", newUser.getBloodGroup());
     query.bindValue(":dob", newUser.getDob());
     query.bindValue(":medicalHistory", newUser.getMedicalHistory());
-    query.bindValue(":consent", newUser.getConsent() ? 1 : 0);
+    query.bindValue(":consent", newUser.getConsent() ? 1 : 0);   // 1 for agreed and 0 for not 
 
     // SQL Error Trap to catch specific rejection reasons
     if (!query.exec()) {
         QMessageBox::critical(nullptr, "Registration Failed", "The database rejected the data. Reason:\n\n" + query.lastError().text());
         return false;
     }
+	// .exec is actually executing the SQL query and if it fails it will show a message box with the specific error message from the database
 
     return true;
 }
@@ -113,6 +133,7 @@ user DatabaseManager::getUserDetails(const QString& username)
     query.prepare("SELECT * FROM User WHERE username = :username");
     query.bindValue(":username", username);
 
+    //.exec() runs the query .next() moves to the first result row If both succeed, theres a matching user in the database
     if (query.exec() && query.next()) {
         return user(
             query.value("username").toString(),
@@ -130,7 +151,7 @@ user DatabaseManager::getUserDetails(const QString& username)
             query.value("consent").toBool()
         );
     }
-    return user();
+	return user(); // if no user found return default user object with empty values
 }
 
 void DatabaseManager::createDashboardTables()
@@ -138,6 +159,26 @@ void DatabaseManager::createDashboardTables()
     db.transaction();
     QSqlQuery query(db);
 
+    //these all tables depend on parent table [user] these all are child tables
+
+    /*
+ * WHY DAILYMETRICS TABLE?
+ * This table acts as a summary (or cache) table to optimize query performance.
+ * Instead of running heavy SQL calculations (SUM, AVG) on thousands of rows
+ * inside the Activity, Vitals, and Nutrition logs to generate historical charts,
+ * we store the pre-calculated daily totals here. This allows the Dashboard
+ * to load instantly by reading just a few summarized rows.
+ */
+
+
+
+ /*
+* Why we didnt add foreign key?
+* 'username' acts as a conceptual foreign key to the 'User' table.
+* Explicit SQL constraints are omitted because:
+* 1. C++ session logic guarantees data integrity (only valid users can insert).
+* 2. SQLite disables foreign key enforcement by default anyway.
+*/
     query.exec(R"(
         CREATE TABLE IF NOT EXISTS DailyMetrics (
             metric_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -223,14 +264,17 @@ void DatabaseManager::createDashboardTables()
 
 DailyMetrics DatabaseManager::getDailyMetrics(const QString& username, const QDate& date)
 {
-    DailyMetrics data = { 0 };
-    QString dateStr = date.toString("yyyy-MM-dd");
+	DailyMetrics data = { 0 };  //c++ trick to meke new object named dailymetrics and initialize all its members to zero or default values
+	QString dateStr = date.toString("yyyy-MM-dd");  // converting QDate to string format which is compatible with our database storage and query
 
     QSqlQuery nutritionQuery(db);
+
+	//calculating the total calories, protein, carbs and fats consumed by the user on a specific date by summing up the values from the NutritionLog table for that user and date
     nutritionQuery.prepare(R"(
         SELECT SUM(calories), SUM(protein_g), SUM(carbs_g), SUM(fats_g) 
         FROM NutritionLog WHERE username = :usr AND record_date = :date
     )");
+
     nutritionQuery.bindValue(":usr", username);
     nutritionQuery.bindValue(":date", dateStr);
 
@@ -242,6 +286,8 @@ DailyMetrics DatabaseManager::getDailyMetrics(const QString& username, const QDa
     }
 
     QSqlQuery activityQuery(db);
+
+   //repeating the same process as above
     activityQuery.prepare(R"(
         SELECT SUM(calories_burned), SUM(step_count) FROM ActivityLog 
         WHERE username = :usr AND record_date = :date
@@ -259,45 +305,57 @@ DailyMetrics DatabaseManager::getDailyMetrics(const QString& username, const QDa
         SELECT heart_rate, sleep_hours FROM VitalsLog 
         WHERE username = :usr AND record_date = :date ORDER BY vital_id DESC LIMIT 1
     )");
+
+	//here desc limit 1 means if there are multiple entries for the same date we will take the latest one which is more relevant for the dashboard display and analysis 
     vitalsQuery.bindValue(":usr", username);
     vitalsQuery.bindValue(":date", dateStr);
 
     if (vitalsQuery.exec() && vitalsQuery.next()) {
         data.heartRate = vitalsQuery.value(0).toInt();
         double totalSleep = vitalsQuery.value(1).toDouble();
+		// converting total sleep hours into hours and minutes format for better readability on the dashboard
         data.sleepHours = static_cast<int>(totalSleep);
         data.sleepMinutes = static_cast<int>((totalSleep - data.sleepHours) * 60);
 
         double recPct = (totalSleep / 8.0) * 100.0;
+		// calculating recovery percentage based on sleep hours with a cap at 100% to avoid unrealistic values
         data.recoveryPercentage = (recPct > 100.0) ? 100 : static_cast<int>(recPct);
     }
 
     return data;
 }
 
+
+//shows it will return a activity record list then it shows that getActivityLog function is member of databasemanager class 
+//giving address so that the functio dont make a copy of username and data in another memory and const used so it becomes read only memory
 QList<ActivityRecord> DatabaseManager::getActivityLog(const QString& username, const QDate& date)
 {
-    QList<ActivityRecord> logList;
+    QList<ActivityRecord> logList; //created empty list of activity record and this will hold multiple activity records for a specific user 
     QSqlQuery query(db);
 
+    //extracting all activity records for a specific user and date from the ActivityLog table
     query.prepare("SELECT * FROM ActivityLog WHERE username = :username AND record_date = :date");
     query.bindValue(":username", username);
     query.bindValue(":date", date.toString("yyyy-MM-dd"));
+    
 
-    if (query.exec()) {
+    if (query.exec()) {// checks wheather the query runs without error
         while (query.next()) {
-            ActivityRecord record;
+            ActivityRecord record;   //creating record object agian and again
             record.exerciseName = query.value("exercise_name").toString();
             record.durationMins = query.value("duration_mins").toInt();
             record.intensity = query.value("intensity").toString();
             record.caloriesBurned = query.value("calories_burned").toInt();
             record.syncStatus = query.value("sync_status").toString();
+
+            //when all exercise data complete in one pack it will save all the data in loglist
             logList.append(record);
         }
     }
-    return logList;
+    return logList; // then it will return the list which will show in table using ui 
 }
 
+//return true if the record is successfully saved in database otherwise false
 bool DatabaseManager::saveActivityRecord(const QString& username, const QDate& date, const QString& exerciseName, int durationMins, int intensity, int caloriesBurned, int steps)
 {
     QSqlQuery query(db);
@@ -305,6 +363,7 @@ bool DatabaseManager::saveActivityRecord(const QString& username, const QDate& d
         INSERT INTO ActivityLog (username, record_date, exercise_name, duration_mins, intensity, calories_burned, step_count, sync_status) 
         VALUES (:username, :date, :exercise, :duration, :intensity, :calories, :steps, 'PENDING')
     )");
+    //initialize all data sync status as pending and when user click on sync button it will show synced 
 
     query.bindValue(":username", username);
     query.bindValue(":date", date.toString("yyyy-MM-dd"));
@@ -314,17 +373,22 @@ bool DatabaseManager::saveActivityRecord(const QString& username, const QDate& d
     query.bindValue(":calories", caloriesBurned);
     query.bindValue(":steps", steps);
 
-    return query.exec();
+    return query.exec();    //perform insertion operation and return true if successful otherwise false
 }
 
 bool DatabaseManager::saveVitalsRecord(const QString& username, const QDate& date, double weight, double bodyTemp, int heartRate, int bpSys, int bpDia, int bloodSugar, int stressLevel, double sleepHours)
 {
+
+    // This ensures the user's main profile always shows their latest weight, instead of the old weight from the day they created the account.
+
     QSqlQuery weightQuery(db);
     weightQuery.prepare("UPDATE User SET weight = :weight WHERE username = :username");
     weightQuery.bindValue(":weight", weight);
     weightQuery.bindValue(":username", username);
     weightQuery.exec();
+    
 
+    //now update the vitals record
     QSqlQuery query(db);
     query.prepare(R"(
         INSERT INTO VitalsLog (username, record_date, weight, body_temp, heart_rate, bp_sys, bp_dia, blood_sugar, stress_level, sleep_hours) 
@@ -342,7 +406,7 @@ bool DatabaseManager::saveVitalsRecord(const QString& username, const QDate& dat
     query.bindValue(":stress", stressLevel);
     query.bindValue(":sleep", sleepHours);
 
-    return query.exec();
+	return query.exec();  
 }
 
 bool DatabaseManager::saveNutritionRecord(const QString& username, const QDate& date, const QString& mealType, const QString& foodName, int calories, int protein, int carbs, int fats, double waterIntake, int caffeine)
@@ -390,6 +454,8 @@ QList<GoalRecord> DatabaseManager::getGoals(const QString& username)
     QList<GoalRecord> goalsList;
     QSqlQuery query(db);
 
+	//here we used columns names with select query but we can also use * but using column names is better for readability and maintainability of code and also it is more efficient as it only fetches the required data instead of fetching all columns which may include unnecessary data
+
     query.prepare("SELECT target_name, category, baseline, threshold, frequency FROM UserGoals WHERE username=:username");
     query.bindValue(":username", username);
 
@@ -410,6 +476,7 @@ QList<GoalRecord> DatabaseManager::getGoals(const QString& username)
 bool DatabaseManager::markDataAsSynced(const QString& username)
 {
     QSqlQuery query(db);
+    // this function updates the sync status of all activities 
     query.prepare("UPDATE ActivityLog SET sync_status = 'SYNCED' WHERE username = :usr AND sync_status = 'PENDING'");
     query.bindValue(":usr", username);
 
